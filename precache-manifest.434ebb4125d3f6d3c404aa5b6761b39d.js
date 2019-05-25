534f22dc403ac510841bb48a9d2fc584f0d7284e
Eduard Benedic <benediceduard@outlook.com>
2019-05-25 08:02:47 +0100

    Updates

---
+++ precache-manifest.434ebb4125d3f6d3c404aa5b6761b39d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a5b97cdf72f877d838b0cd59791915",
+    "url": "/Eduard-Benedic/react-portfolio.git/index.html"
+  },
+  {
+    "revision": "70ef2156bd8d4ff10092",
+    "url": "/Eduard-Benedic/react-portfolio.git/static/css/main.72cb9c6e.chunk.css"
+  },
+  {
+    "revision": "25f8f29a2cb5511380e7",
+    "url": "/Eduard-Benedic/react-portfolio.git/static/js/2.855eeeb6.chunk.js"
+  },
+  {
+    "revision": "70ef2156bd8d4ff10092",
+    "url": "/Eduard-Benedic/react-portfolio.git/static/js/main.292bd59b.chunk.js"
+  },
+  {
+    "revision": "c99e3a0f8553fd428e3f",
+    "url": "/Eduard-Benedic/react-portfolio.git/static/js/runtime~main.8b9f3b76.js"
+  },
+  {
+    "revision": "1bc9c2037e9c004b0075875354ed03de",
+    "url": "/Eduard-Benedic/react-portfolio.git/static/media/img-back-large.1bc9c203.jpg"
+  },
+  {
+    "revision": "0ce541bb7fd2937fe8fe87fb991d7f69",
+    "url": "/Eduard-Benedic/react-portfolio.git/static/media/img-website.0ce541bb.png"
+  },
+  {
+    "revision": "5d67ea9181337e7aefc8a8015c8b1ef8",
+    "url": "/Eduard-Benedic/react-portfolio.git/static/media/img2.5d67ea91.jpg"
+  }
+]);
